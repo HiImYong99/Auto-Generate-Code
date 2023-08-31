7f5fbbcdb996ae4d688d6bf21c5a2c49ed738a66
HiImYong99 <good19422@naver.com>
2023-08-31 21:22:05 +0900

    23.08.31 JS 구현 (리팩토링 필수)

--- src/js/app.js
+++ src/js/app.js
@@ -15,10 +15,12 @@ let question;
 // 화면에 뿌려줄 데이터, 질문들
 let questionData = [];
 
+//사용자에게 입력받은 요청을 data에 넣음
+
 const sendQuestion = (lang, method, detail) => {
   data.push({
     role: "user",
-    content: `프로그래밍 언어는 ${lang}이고 ${detail}이걸 ${method} 해줘`,
+    content: `프로그래밍 언어는 ${lang}이고 ${detail}이걸 ${method} 해줘 `,
   });
   questionData.push({
     role: "user",
@@ -26,25 +28,13 @@ const sendQuestion = (lang, method, detail) => {
   });
 };
 
-// 화면에 질문 그려주는 함수
-
-const printQuestion = async () => {
-  let p = document.createElement("p");
-  p.classList.add("question");
-  questionData.map(el => {
-    p.innerText = el.content;
-  });
-  $input_text.appendChild(p);
-  questionData = [];
-  question = false;
-};
+// api에게 받은 답변을 화면에 출력함
 
 const printAnswer = async answer => {
   let p = document.createElement("p");
   p.classList.add("answer");
   console.log(p);
   $output_text.textContent = answer;
-  // $output_text.appendChild(p);
 };
 
 const apiPost = async () => {
@@ -70,10 +60,14 @@ $form.addEventListener("submit", e => {
   const lang_value = $input_lang.options[$input_lang.selectedIndex].value;
   const method_value = $input_method.options[$input_method.selectedIndex].value;
   const detail_value = $input_text.value;
-
+  const load = document.createElement("img");
+  alert("요청되었습니다. 잠시만 기다려주세요.");
+  load.setAttribute("src", "asset/img/loading.gif");
+  $output_text.innerHTM = load;
   sendQuestion(lang_value, method_value, detail_value);
   apiPost();
-  printQuestion();
+  // printQuestion();
+  save_Item(lang_value, method_value, detail_value);
 });
 
 $btn.addEventListener("click", e => {
@@ -82,3 +76,32 @@ $btn.addEventListener("click", e => {
     "https://www.ryugod.com/pages/ide/" +
     $input_lang.options[$input_lang.selectedIndex].value;
 });
+
+// 로컬 스토리지에 저장
+
+function save_Item(lang, method, detail) {
+  let lang_arr = load_Item()[0];
+  let method_arr = load_Item()[1];
+  // let detail_arr = load_Item()[2];
+
+  lang_arr.push(lang);
+  method_arr.push(method);
+  // detail_arr.push(detail);
+
+  localStorage.setItem("lang", JSON.stringify(lang_arr));
+  localStorage.setItem("method", JSON.stringify(method_arr));
+  // localStorage.setItem("detail", JSON.stringify(detail_arr));
+}
+
+function load_Item() {
+  let lang = localStorage.getItem("lang")
+    ? JSON.parse(localStorage.getItem("lang"))
+    : [];
+  let method = localStorage.getItem("method")
+    ? JSON.parse(localStorage.getItem("method"))
+    : [];
+  let detail = localStorage.getItem("detail")
+    ? JSON.parse(localStorage.getItem("detail"))
+    : [];
+  return [lang, method, detail];
+}
---
+++ src/js/mypage.js
@@ -0,0 +1,69 @@
+const $use_count = document.querySelector("#use-count");
+const $lang_count = document.querySelector("#lang-count");
+const $method_count = document.querySelector("#method-count");
+const $remove_btn = document.querySelector("#local-remove");
+
+function load_Item() {
+  let lang = localStorage.getItem("lang")
+    ? JSON.parse(localStorage.getItem("lang"))
+    : [];
+  lang ? console.log(lang) : console.log("없어");
+  let method = localStorage.getItem("method")
+    ? JSON.parse(localStorage.getItem("method"))
+    : [];
+  let detail = localStorage.getItem("detail")
+    ? JSON.parse(localStorage.getItem("detail"))
+    : [];
+  if (lang.length === 0) {
+    console.log("빈배열 실행");
+
+    $use_count.innerText = "0회";
+    $lang_count.innerText = "0회";
+    $method_count.innerText = "0회";
+  } else {
+    write_count(lang, method, detail);
+    return [lang, method, detail];
+  }
+}
+
+function write_count(lang, method, detail) {
+  let most_lang_name = getSortedArr(lang)[0][0];
+  let most_lang_count = getSortedArr(lang)[0][1];
+  let most_method_name = getSortedArr(method)[0][0];
+  let most_method_count = getSortedArr(method)[0][1];
+  
+  $use_count.innerText = lang.length + "회";
+  $lang_count.innerText = `${most_lang_name} ( ${most_lang_count}회 )`;
+  $method_count.innerText = `${most_method_name} ( ${most_method_count}회 )`;
+}
+load_Item();
+
+$remove_btn.addEventListener("click", () => {
+  if (confirm("정말 사용했던 기록을 초기화 하시겠습니까?")) {
+    localStorage.removeItem("lang");
+    localStorage.removeItem("method");
+    localStorage.removeItem("detail");
+  }
+  load_Item();
+});
+
+function getSortedArr(array) {
+  // 1. 출연 빈도 구하기
+  const counts = array.reduce((pv, cv) => {
+    pv[cv] = (pv[cv] || 0) + 1;
+    return pv;
+  }, {});
+
+  // 2. 요소와 개수를 표현하는 배열 생성 => [ [요소: 개수], [요소: 개수], ...]
+  const result = [];
+  for (let key in counts) {
+    result.push([key, counts[key]]);
+  }
+
+  // 3. 출현 빈도별 정리하기
+  result.sort((first, second) => {
+    return second[1] - first[1];
+  });
+
+  return result;
+}
