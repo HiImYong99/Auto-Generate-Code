176bf91e9c623ced0fff65c3feee74aa90b3723f
HiImYong99 <good19422@naver.com>
2023-09-06 22:04:48 +0900

    23.09.06 주석 수정

--- src/js/mypage.js
+++ src/js/mypage.js
@@ -26,19 +26,19 @@ function load_count() {
 
 // 저장한 데이터 정렬 및 최빈값 리턴
 function getSortedArr(array) {
-  // 1. 출연 빈도 구하기
+  // 출연 빈도 
   const counts = array.reduce((pv, cv) => {
     pv[cv] = (pv[cv] || 0) + 1;
     return pv;
   }, {});
 
-  // 2. 요소와 개수를 표현하는 배열 생성 => [ [요소: 개수], [요소: 개수], ...]
+  // 요소와 개수를 표현하는 배열 생성 
   const result = [];
   for (let key in counts) {
     result.push([key, counts[key]]);
   }
 
-  // 3. 출현 빈도별 정리하기
+  // 출현 빈도별 정리
   result.sort((first, second) => {
     return second[1] - first[1];
   });
